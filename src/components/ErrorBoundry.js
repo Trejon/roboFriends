8c484762e93963b7a94b6c00a9b6b5a9608632da
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-05-19 15:43:27 -0600

    added error boundry

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      hasError: false
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true
+    })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Oooopps. That is not good.</h1>
+        </div>
+      )
+    }
+      return this.props.children
+  }
+}
+
+export default ErrorBoundry;
