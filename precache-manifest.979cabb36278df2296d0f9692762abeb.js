cd29400820ec75f2590ff202fff963f4266f3695
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-05-19 16:12:36 -0600

    Updates

---
+++ precache-manifest.979cabb36278df2296d0f9692762abeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2802de19777c0f7a0ca461f00f3aab",
+    "url": "/roboFriends/index/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/index/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eff59297ca9d641fe6f6",
+    "url": "/roboFriends/index/static/css/main.ccc6efd6.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/index/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/index/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff59297ca9d641fe6f6",
+    "url": "/roboFriends/index/static/js/main.155c32d5.chunk.js"
+  },
+  {
+    "revision": "05efb251fee4f8bca5d2",
+    "url": "/roboFriends/index/static/js/runtime-main.881c02bd.js"
+  }
+]);
