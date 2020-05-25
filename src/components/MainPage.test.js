9c3b3467d58761f0456e5990e87240d09656cbcb
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-05-25 12:03:19 -0600

    added tests

---
+++ src/components/MainPage.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MainPage from '../components/MainPage';
+import { exportAllDeclaration } from '@babel/types';
+
+let wrapper;
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [], 
+    searchFields: '', 
+    isPending: false
+  }
+  wrapper = shallow(<MainPage {...mockProps} />)
+})
+
+it('renders MainPage without crashing', () => {
+  expect(wrapper).toMatchSnapshot();
+})
+
+it('filters robots correctly', () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots: [{
+      id: 3, 
+      name: 'John', 
+      email: 'john@gmail.com'
+    }], 
+    searchField: 'John', 
+    isPending: false
+  }
+  const wrapper2 = shallow(<MainPage {...mockProps2} />)
+  expect(wrapper2.instance().filterRobots()).toEqual([{
+    id: 3, 
+    name: 'John', 
+    email: 'john@gmail.com'
+  }])  
+})
+
+it('filters robots correctly 2', () => {
+  const mockProps3 = {
+    onRequestRobots: jest.fn(),
+    robots: [{
+      id: 3, 
+      name: 'John', 
+      email: 'john@gmail.com'
+    }], 
+    searchField: 'a', 
+    isPending: false
+  }
+  const filteredRobots = []
+  const wrapper2 = shallow(<MainPage {...mockProps3} />)
+  expect(wrapper2.instance().filterRobots()).toEqual(filteredRobots)  
+})
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import Header from '../components/Header';
-import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 import { connect } from 'react-redux'
 import { setSearchField, requestRobots } from '../actions/actions';
+import MainPage from '../components/MainPage';
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          { isPending ? <h1>Loading</h1> :
-            <ErrorBoundry>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundry>
-          }
-        </Scroll>
-      </div>
-    );
+    return(
+      <MainPage {...this.props} />
+    )
   }
 }
 
